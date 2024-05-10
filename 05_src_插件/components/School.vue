d0752dca1642a107ca53671e4a59a0a1c3ea6432
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-05-10 13:47:05 +0800

    Add files via upload

---
+++ 05_src_插件/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学生地址：{{ address }}</h2>
+    <button @click="test">点我测试一个hello方法</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+      name:'School',
+      data() {
+          return {
+              name:'sc',
+              address:'tc'
+          }
+      },
+      methods:{
+        test(){
+          this.hello()
+        }
+      }
+  }
+</script>
---
+++ 05_src_插件/components/Student.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>学生名称：{{ name | mySlice }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <input type="text" v-fbind:value="name">
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data() {
+            return {
+                name:'张三lsb1021',
+                sex:'男'
+            }
+        },
+    }
+</script>
