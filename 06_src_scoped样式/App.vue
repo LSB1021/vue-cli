09449be1a26dd460ed2c52f26e577a2707eeda93
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-05-10 16:35:19 +0800

    Add files via upload

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="title">你好啊</h1>
+    <School/>
+    <hr>
+    <Student/>
+  </div>
+</template>
+
+<script>
+    import Student from './components/Student'
+    import School from './components/School'
+
+    export default {
+        name:'App',
+        components:{
+          Student,
+          School
+        },
+    }
+</script>
+
+<style>
+  .title{
+    color: red;
+  }
+</style>
