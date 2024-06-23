6b769fec79fc506f966821b95f5efe7db2f7ad7f
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-06-23 19:28:46 +0800

    Add files via upload

---
+++ 19_src_配置代理服务器/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="root">
+		<div>
+			<button @click="getStudent">获取学生信息</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+    export default {
+        name:'App',
+		methods:{
+			getStudent(){
+				axios.get('http://localhost:8080/lsb/shop/index/allCategory').then(
+					response =>{
+						console.log('请求成功了',response.data);
+					},
+					error =>{
+						console.log('请求失败了',error.message);
+						
+					}
+				)
+			},
+		}
+    }
+</script>
