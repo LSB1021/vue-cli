73810170a0bd1cb13abdee11e5f87de039067626
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-06-24 10:31:21 +0800

    Add files via upload

---
+++ 21_src_github搜索案例_vue-resource/components/Search.vue
@@ -0,0 +1,40 @@
+<template>
+	<section class="jumbotron">
+		<h3 class="jumbotron-heading">Search Github Users</h3>
+		<div>
+			<input type="text" placeholder="enter the name you search" v-model="keyWord"/>&nbsp;
+			<button @click="searchUsers">Search</button>
+		</div>
+	</section>
+</template>
+
+<script>
+	// import axios from 'axios'
+	export default {
+		name:'Search',
+		data() {
+			return {
+				keyWord:''
+			}
+		},
+		methods: {
+			searchUsers(){
+				//请求前更新List的数据
+				this.$bus.$emit('updateListData',{isLoading:true,errMsg:'',users:[],isFirst:false})
+				// axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+				this.$http.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+					response => {
+						console.log('请求成功了')
+						//请求成功后更新List的数据
+						this.$bus.$emit('updateListData',{isLoading:false,errMsg:'',users:response.data.items})  //response.data.items为获取对象数据遍历键值对
+					},
+					error => {
+						//请求后更新List的数据
+						this.$bus.$emit('updateListData',{isLoading:false,errMsg:error.message,users:[]})  //请求失败一定要把users:[]设为空，清空上一次数据
+					}
+				)
+				
+			}
+		},
+	}
+</script>
---
+++ 21_src_github搜索案例_vue-resource/main.js
@@ -0,0 +1,19 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+//引入vue-resource插件
+import vueResource from 'vue-resource'  //使用此插件了解即可
+//关闭Vue生产提示
+Vue.config.productionTip = false
+//使用插件
+Vue.use(vueResource)  //使用后vc里多了$http
+
+//创建vm
+new Vue({
+    el:'#app',
+    render:h => h(App),
+    beforeCreate(){
+        Vue.prototype.$bus = this
+    }
+})
