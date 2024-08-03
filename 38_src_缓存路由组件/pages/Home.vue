7774b66ca604bbe963c1dbb91d774be9e14d84f9
LSB1021 <118779507+LSB1021@users.noreply.github.com>
2024-08-03 09:38:09 +0800

    Add files via upload

---
+++ 38_src_缓存路由组件/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Home组件内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link replace class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link replace class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <keep-alive include="News"> <!--此处News为组件名（组件内部定义的name的值）-->
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'Home',
+        beforeDestroy(){
+          console.log('Home组件即将被销毁');
+        },
+        /* mounted(){
+          console.log('Home组件挂载完毕了',this);//this中发现组件中多了$route和$router
+          window.homeRoute = this.$route
+          window.homeRouter = this.$router
+        } */
+    }
+</script>
